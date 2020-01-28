bd26ca2d170e05ef0574698614d6a391abacc236
Carl Aiau <carl@ortus.co.nz>
2020-01-28 23:41:36 +1300

    synchronicity fixes

--- acquisiton/scraper.js
+++ acquisiton/scraper.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio')
 const fs = require('fs');
 const axios = require('axios');
+const Promise = require('bluebird')
 
 function generateList(outputFileName){
     axios.get('https://labdoor.com/rankings/fish-oil/quality')
@@ -28,47 +29,32 @@ function generateList(outputFileName){
 function processList(input, output, limit = false){
     fs.readFile(input, (err, data) => {
         if (err) throw err;
-        let oils = JSON.parse(data);
-        
-        let promises = []
-            
-        for(let i = 0; i < (limit && limit < oils.length ? limit : oils.length); i++){
-            const getSinglePage = new Promise( (resolve) => {
-                
-                processSingle(oils[i].link).then( (result) => {
-                    resolve( {...oils[i], ...result} )
-                }).catch(() => {                    
-                    resolve()
-                })
-            })
-            promises.push(getSinglePage)
-        }
 
-        Promise.all(promises).then( (results) => {
+        Promise.map(JSON.parse(data), oil => {
+            return new Promise(resolve => {
+                console.log(oil.title + " has begun!")
+                processSingle(oil).then( (result) => {
+                    resolve(result)
+               });
+            });
+        }, {concurrency: 2})
+        .then( (results) => {
             fs.writeFile(output, JSON.stringify(results, null, 2), (err) => {
                 if(err) return console.log(err);
                 console.log("The list was saved!");
             });
         })
+
+
         
     });
 }
-function processSingle(url){
+function processSingle(oil){
     return new Promise( (resolve) => {
-        axios.get(url)
+        axios.get(oil.link)
         .then(response => {
             const $ = cheerio.load(response.data)
-            info = {}
-            $('ul.keyDataList li.keyDataItem').each( (i, el) => {
-                key = $(el).find('.keyDataKey').text().trim()
-                value = $(el).text().trim().replace(key, '').replace(/[^A-Za-z0-9\-\(\) .]/g, '')
-                info[key.replace(/[ \-\/]/g, "_").toLowerCase()] = value
-            })
-
-            images = {}
-            $('.flexCol.flexCenter img').each( (i, el) => { images[i] = $(el).attr('src'); })
-            
-            promises = []
+            let promises = []
             $('.buyNowWrapper a').each( (i, el) => {
                 const getProductLink = new Promise( (resolveIn) => {
                     const provider = $(el).text().trim().replace(/[^A-Za-z0-9\-\(\).]/g, '')    
@@ -84,16 +70,38 @@ function processSingle(url){
                 promises.push(getProductLink)
             })
             
-            Promise.all(promises).then( (links) => {
-                resolve({info, images, links})
+            Promise.all(promises)
+            .then( (links) => {
+                
+                resolve({...oil, ...syncContent($), links})
             })
             .catch(() => {
-                resolve({info, images})
+                resolve({...oil, ...syncContent($)})
             })
         })
         
     })
 }
+
+function syncContent($){
+    const info = {}
+    $('ul.keyDataList li.keyDataItem').each( (i, el) => {
+        key = $(el).find('.keyDataKey').text().trim()
+        value = $(el).text().trim().replace(key, '').replace(/[^A-Za-z0-9\-\(\) .]/g, '')
+        info[key.replace(/[ \-\/]/g, "_").toLowerCase()] = value
+    })
+
+    let images = []
+    $('.flexCol.flexCenter img').each( (i, el) => { 
+        images.push($(el).attr('src'))
+    })
+
+    return {info, images}
+}
+
+
+
+
 
 // Main thread. Gross
 (() => {
