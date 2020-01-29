d82dcb942293583a9c7de256bae93f95ce75adaa
Carl Aiau <carl@ortus.co.nz>
2020-01-29 13:43:19 +1300

    Initial Output to Tables

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,21 +1,78 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+
+
+const IndexPage = ( {data} ) => {
+  console.log(data.allStrapiOil.edges)
+
+  const oils = data.allStrapiOil.edges.map( ({node}) => {
+    const {Image, Title, DHA, EPA, ServingSize, CapsulesPerContainer, iherb_link, iherb_price} = node
+    return (
+      <tr>
+        <td><img src={`${Image.publicURL}`} style={{width: '150px'}}/></td>
+        <td>{Title}</td>
+        <td>{DHA}</td>
+        <td>{EPA}</td>
+        <td>{ServingSize}</td>
+        <td>{CapsulesPerContainer}</td>
+        <td>
+          <a href={iherb_link} target="_blank" rel="noopener noreferrer">
+            ${iherb_price.toFixed(2)}
+          </a>
+        </td>
+        
+      </tr>
+    )
+  });
+
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <table>
+        <thead>
+          <tr>
+            <td></td>
+            <td>Title</td>
+            <td>DHA</td>
+            <td>EPA</td>
+            <td>Serving Size</td>
+            <td>Capsules Per Container</td>
+            <td>Price (iHerb)</td>
+          </tr>
+        </thead>
+        <tbody>
+          {oils}
+        </tbody>
+      </table>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allStrapiOil {
+      edges {
+        node {
+          Title
+          iherb_link
+          iherb_price
+          ServingSize
+          DHA
+          EPA
+          CapsulesPerContainer
+          Image {
+            publicURL
+          }
+        }
+      }
+    }
+  }
+`
