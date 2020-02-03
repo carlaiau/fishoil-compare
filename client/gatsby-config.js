4349efb6f40e50d433e5f11e9e2fbbaea9ec401e
Carl Aiau <carl@ortus.co.nz>
2020-02-03 23:54:24 +1300

    ngrok should be deployable

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `http://028469cb.ngrok.io:1337`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`oil`]
       },
